c6b45220b94775257a6eb7a829dd9496f3a3124b
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-06-01 04:58:29 +0000

    e

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
-import heapq
+def isPossible(stones, k, mid):
+    count = 0
+    for stone in stones:
+        if stone - mid <= 0:
+            count += 1
+        else:
+            count = 0
 
-def dijkstra(graph, start):
-    n = len(graph)
-    dist = [float('inf')] * n
-    dist[start] = 0
-    q = [(0, start)]
-    while q:
-        cost, u = heapq.heappop(q)
-        if dist[u] < cost:
-            continue
-        for v, w in graph[u]:
-            if new_cost := dist[u] + w; new_cost < dist[v]:
-                dist[v] = new_cost
-                heapq.heappush(q, (new_cost, v))
-    return dist
+        if count >= k:
+            return False
+    return True
 
-graph = [
-    [(1, 2), (2, 5)],
-    [(0, 2), (2, 4), (3, 6)],
-    [(0, 5), (1, 4), (3, 2), (4, 3)],
-    [(1, 6), (2, 2), (4, 7)],
-    [(2, 3), (3, 7), (5, 1)],
-    [(4, 1)],
-]
-start = 0
-dist = dijkstra(graph, start)
-for i, d in enumerate(dist):
-    print(f"노드 {i}까지의 최단 거리: {d}")
+def solution(stones, k):
+    copy = sorted(stones)
+
+    low, high = 1, copy[-1]
+
+    while low <= high:
+        mid = (low + high) // 2
+        if isPossible(stones, k, mid):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return low
